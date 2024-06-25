72e9586bd6a88cfb4652f1305d1aca5ca8a187dd
Rajneeshzytox <Rajneeshzytox@users.noreply.github.com>
2024-06-25 16:02:41 +0000

    deploy: 3ba63a87ac7d7db424b6736b0e0d82e182bf36d1

---
+++ src/main.js
@@ -0,0 +1,70 @@
+
+
+new TypeIt("#typeMain", { 
+    lifeLike: false, 
+    speed: 0 
+})
+    .type("a")
+	.pause(580)
+	.type("i")
+	.pause(568)
+	.delete(1)
+	.pause(222)
+	.delete(1)
+	.pause(567)
+	.type("A")
+	.pause(574)
+	.type("i")
+	.pause(560)
+	.type(" ")
+	.pause(509)
+	.type("C")
+	.pause(295)
+	.type("a")
+	.pause(506)
+	.type("l")
+	.pause(500)
+	.type("c")
+	.pause(306)
+	.type("u")
+	.pause(500)
+	.type("l")
+	.pause(164)
+	.type("a")
+	.pause(509)
+	.type("t")
+	.pause(219)
+	.type("e")
+	.pause(126)
+	.type("r")
+	.pause(500)
+	.delete(1)
+	.pause(168)
+	.delete(1)
+	.pause(776)
+	.type("o")
+	.pause(139)
+	.type("r")
+	.go();
+
+
+	// nav mobile 
+
+	let navBtn = document.getElementById('navBtn');
+	let closeNavBtn = document.getElementById('closeNavBtn');
+	let navItems = document.getElementById('navItems');
+
+	navBtn.addEventListener("click", (e) =>{
+		navItems.style.opacity = "1";
+		// navItems.style.scale = "1";
+		navItems.style.pointerEvents = "all";
+
+		// alert("btn clicked");
+	});
+	closeNavBtn.addEventListener("click", (e) =>{
+		navItems.style.opacity = "0";
+		// navItems.style.scale = "0";
+		navItems.style.pointerEvents = "none";
+
+		// alert("btn clicked");
+	});
